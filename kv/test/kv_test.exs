75183e187db942fbd4869dcc7adbcfb3863901a8
xdNuggets <joshpatg2005@gmail.com>
2026-05-18 14:34:28 +0100

    test: rewrite KV tests in ExUnit

--- kv/test/kv_test.exs
+++ kv/test/kv_test.exs
@@ -1,4 +1,25 @@
 defmodule KvTest do
   use ExUnit.Case
-  doctest Kv
+  # disable doctests so i can easily see the tests i make here
+  # doctest Kv
+
+  test "create new KV" do
+    assert Kv.new() == []
+  end
+
+  test "add value to new KV" do
+    assert Kv.put(5, Kv.new()) == [5]
+  end
+
+  test "add value to existing KV" do
+    assert Kv.put(5, [1, 2, 3]) == [5, 1, 2, 3]
+  end
+
+  test "delete value from KV" do
+    assert Kv.delete([1, 2, 3]) == [2, 3]
+  end
+
+  test "get first value of KV" do
+    assert Kv.get([1, 2, 3]) == 1
+  end
 end
